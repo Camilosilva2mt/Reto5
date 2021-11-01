4b662b8543404c20a324a54d45500ab2f94d686c
Camilosilva2mt <camilo.silva2.mt@correo.usa.edu.co>
2021-11-01 00:57:04 -0500

    first commit

---
+++ src/main/java/Reto3Grupo/Reto3/InterfaceCategoria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3Grupo.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ *
+ * @author Camilo Andres Silva A.
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer> {
+    
+}
---
+++ src/main/java/Reto3Grupo/Reto3/InterfaceReservaciones.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3Grupo.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Camilo Andres Silva A.
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Integer>  {
+    
+}
